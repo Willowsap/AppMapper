77b58a4d3a7c3493ee1c1bcd8b35010bf2a0f4ed
Willowsap <willowsapphire@protonmail.com>
2020-12-02 14:44:15 -0500

    testing different index.js

--- scripts/index-old.js
+++ scripts/index-old.js
@@ -9,18 +9,14 @@
   chart.container("container").draw();
 })*/
 
-// let xmlhttp = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function() {
-//   if (this.readyState == 4 && this.status == 200) {
-//     document.getElementById("container").appendChild(chart(JSON.parse(this.responseText)));
-//   }
-// };
-// xmlhttp.open("GET", "https://student2.cs.appstate.edu/sapphirewe/AppMapper/scripts/d3data.json", true);
-// xmlhttp.send();
-
-// function populateData() {
-//   document.getElementById("container").appendChild(chart(getData()));
-// }
+let xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("container").appendChild(chart(JSON.parse(this.responseText)));
+  }
+};
+xmlhttp.open("GET", "https://student2.cs.appstate.edu/sapphirewe/AppMapper/scripts/d3data.json", true);
+xmlhttp.send();
 
 function chart(data) {
   const height = window.innerHeight;
@@ -31,20 +27,8 @@ function chart(data) {
       .force("link", d3.forceLink(links).id(d => d.id))
       .force("charge", d3.forceManyBody())
       .force("center", d3.forceCenter(width / 2, height / 2));
-
-  const tooltipDiv = d3
-    .select('body')
-    .append('div')
-    .style('position', 'absolute')
-    .style('z-index', '1')
-    .style('visibility', 'hidden')
-    .style('background-color', 'white')
-    .style('pointer-events', 'none')
-    .text('test text')
-
   const svg = d3.create("svg")
     .attr("viewBox", [0, 0, width, height]);
-
   const link = svg.append("g")
     .attr("stroke", "#999")
     .attr("stroke-opacity", 0.6)
@@ -52,54 +36,17 @@ function chart(data) {
     .data(links)
     .join("line")
     .attr("stroke-width", d => Math.sqrt(d.value));
-
   const node = svg.append("g")
     .attr("stroke", "#fff")
     .attr("stroke-width", 1.5)
     .selectAll("circle")
     .data(nodes)
     .join("circle")
-    .attr('class', 'node')
-    .attr("r", 10)
+    .attr("r", 5)
     .attr("fill", color)
-    .on('mouseover', function(d, i)  {
-      console.log('node: ', nodes[i.index]);
-      tooltipDiv
-        .style('visibility', 'visible')
-        .text(nodes[i.index].id);
-    })
-    .on('mousemove', function(d) {
-      tooltipDiv
-        .style('left', (d.pageX) + "px")
-        .style('top', (d.pageY) + "px");
-    })
-    .on('mouseout', function(d) {
-      tooltipDiv
-        .style('visibility', 'hidden');
-    })
     .call(drag(simulation));
-
-  d3.select('body').select('svg').selectAll('g').selectAll('.node')
-    .on('mouseover', function(d)  {
-      console.log(d)
-      tooltipDiv
-        .style('visibility', 'visible')
-    })
-    .on('mousemove', function(d) {
-      console.log(d)
-      tooltipDiv
-        .style('left', (d.pageX) + "px")
-        .style('top', (d.pageY) + "px");
-    })
-    .on('mouseout', function(d) {
-      console.log(d)
-      tooltipDiv
-        .style('visibility', 'hidden');
-    })
-
   node.append("title")
       .text(d => d.id);
-
   simulation.on("tick", () => {
     link
       .attr("x1", d => d.source.x)
@@ -138,5 +85,3 @@ const color = () => {
   const scale = d3.scaleOrdinal(d3.schemeCategory10);
   return d => scale(d.group);
 }
-
-document.getElementById("container").appendChild(chart(getData()));
